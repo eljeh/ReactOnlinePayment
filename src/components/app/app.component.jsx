37dc9a11ca6c198117b953e192bb614c0119f8c7
Ed <edbastarache@gmail.com>
2017-12-03 18:18:56 -0700

    small changes

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -23,16 +23,15 @@ const App = (props) => {
       <div className="row form-field-wrapper">
         <div id="paymentInfo" className="large-12 column">
           <div id="feedback"></div>
-          <div className="row">
-            <div id="paymentTitle" className="large-12 columns">
-                <span className="fa-stack fa-2x">
-                    <span className="fa fa-square fa-stack-2x"></span>
-                    <span className="fa fa-lock fa-stack-1x fa-inverse"></span>
-                    {/* <span className="fa fa-thumbs-up fa-stack-1x fa-inverse"></span> */}
-                </span>
-                <h3>Secure Payment</h3>
-                {/* <h3>Please review your payment information before subitting</h3> */}
-            </div>
+
+          <div id="paymentTitle" className="large-12 columns">
+              <span className="fa-stack fa-2x">
+                  <span className="fa fa-square fa-stack-2x"></span>
+                  <span className="fa fa-lock fa-stack-1x fa-inverse"></span>
+                  {/* <span className="fa fa-thumbs-up fa-stack-1x fa-inverse"></span> */}
+              </span>
+              <h3>Secure Payment</h3>
+              {/* <h3>Please review your payment information before subitting</h3> */}
           </div>
 
           <div id="payment-inputs" className="large-12 columns">
